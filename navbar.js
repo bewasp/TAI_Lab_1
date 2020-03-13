f5a3e804ea83e49efef753be50e8e94e1d794d44
bewasp <beewasp1997@gmail.com>
2020-03-13 16:12:48 +0100

    navbar component

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<a class="nav-link" href="index.html">Home</a>' +
+    '<a class="nav-link" href="#">Quiz</a>' +
+    '<a class="nav-link" href="contact.html">Kontakt</a>';
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
